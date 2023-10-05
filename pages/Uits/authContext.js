9c95c4402cf4639b092808789ac26d8e4aeea0aa
Prosit Kumar Das <109513705+PrositKD@users.noreply.github.com>
2023-10-05 10:28:09 +0600

    Update authContext.js

--- pages/Uits/authContext.js
+++ pages/Uits/authContext.js
@@ -1,3 +1,63 @@
+
+import { createContext, useContext, useState, useEffect } from 'react';
+import axios from 'axios';
+
+
+const AuthContext = createContext();
+
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem('user'));
+    if (storedUser) {
+      setUser(storedUser);
+      console.log('i am locatl');
+    }
+  }, []);
+ 
+  const logindata = (email, cookie) => {
+    const newUser = { email, cookie };
+    setUser(newUser);
+
+    localStorage.setItem('user', JSON.stringify(newUser));
+  };
+
+  const logout = () => {
+    setUser(null);
+    localStorage.removeItem('user');
+   TriggerLogout();
+  };
+
+ 
+
+  async function TriggerLogout() {
+    try {
+      const response = await axios.post(process.env.NEXT_PUBLIC_DELIVERY + 'signout/',
+              {
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+                withCredentials: true
+              }
+            );
+      console.log(response)
+        setUser(null);
+        document.cookie = null;
+
+       
+
+    } catch (error) {
+      console.error('error failed: ', error);
+    }
+  }
+  return (
+    <AuthContext.Provider value={{ user, logindata, logout}}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
 // import { createContext, useContext, useState } from 'react';
 // import { useRouter } from 'next/router';
 // import axios from 'axios';
@@ -66,62 +126,3 @@
 // };
 
 // export const useAuth = () => useContext(AuthContext);
-import { createContext, useContext, useState, useEffect } from 'react';
-import axios from 'axios';
-
-
-const AuthContext = createContext();
-
-
-export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
-
-  useEffect(() => {
-    const storedUser = JSON.parse(localStorage.getItem('user'));
-    if (storedUser) {
-      setUser(storedUser);
-      console.log('i am locatl');
-    }
-  }, []);
- 
-  const logindata = (email, cookie) => {
-    const newUser = { email, cookie };
-    setUser(newUser);
-
-    localStorage.setItem('user', JSON.stringify(newUser));
-  };
-
-  const logout = () => {
-    setUser(null);
-    localStorage.removeItem('user');
-   TriggerLogout();
-  };
-
- 
-
-  async function TriggerLogout() {
-    try {
-      const response = await axios.post(process.env.NEXT_PUBLIC_DELIVERY + 'signout/',
-              {
-                headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-                withCredentials: true
-              }
-            );
-      console.log(response)
-        setUser(null);
-        document.cookie = null;
-
-       
-
-    } catch (error) {
-      console.error('error failed: ', error);
-    }
-  }
-  return (
-    <AuthContext.Provider value={{ user, logindata, logout}}>
-      {children}
-    </AuthContext.Provider>
-  );
-};
-
-export const useAuth = () => useContext(AuthContext);
